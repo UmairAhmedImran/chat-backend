5947a98b1892456829d05f3efda876e424a217d0
ScriptersDev <developerscripters@gmail.com>
2024-09-14 13:03:28 +0500

    Adding frontend in cors

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://chat-frontend-pink-phi.vercel.app/',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
